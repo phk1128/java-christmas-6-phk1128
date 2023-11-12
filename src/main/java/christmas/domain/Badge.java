3a9a273d20417a43c898d565cbcf8ef130649f5c
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-12 22:20:13 +0900

    feat: Badge 도메인 구현 완료
    테스트 통과 목록
    - 총 할인 금액으로 해당하는 배지를 찾는다.

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,27 @@
+package christmas.domain;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public enum Badge {
+
+    별(-5000),
+    트리(-10000),
+    산타(-20000);
+
+
+    private final int totalDiscount;
+
+    Badge(int totalDiscount) {
+        this.totalDiscount = totalDiscount;
+    }
+
+    public static String findByTotalDiscount(int totalDiscount) {
+
+        return Arrays.stream(Badge.values())
+                .filter(badge -> badge.totalDiscount >= totalDiscount)
+                .min(Comparator.comparingInt(badge -> badge.totalDiscount - totalDiscount))
+                .map(badge -> badge.name())
+                .orElse("없음");
+    }
+}
