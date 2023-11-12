65bfdcacf3f3927f75503245e9e38249599a47ea
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-12 22:05:04 +0900

    feat(Benefit, BenefitTest): Benefit 도메인 구현 및 테스트 완료
    테스트 통과 목록
    - 할인 받은 총 금액을 계산한다.
    - 혜택 내역을 출력 형식에 맞게 변환한다.
    - 혜택 받은 금액이 0원 이므로 내역은 없음을 반환한다.
    - 증정 이벤트 할인 가격을 계산한다.
    - 증정 메뉴 내역을 출력 형식에 맞게 변환한다.

---
+++ src/main/java/christmas/domain/Benefit.java
@@ -0,0 +1,70 @@
+package christmas.domain;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Benefit {
+
+    private final Map<Event, Integer> details;
+
+    public Benefit() {
+        this.details = new EnumMap<>(Event.class);
+        initialBenefitDetails();
+    }
+
+    public int calculateTotalDiscount() {
+        return details.values()
+                .stream()
+                .reduce(0, Integer::sum);
+    }
+
+    public void addDiscount(Event event, int discount) {
+        details.computeIfPresent(event, (key, value) -> value + discount);
+    }
+
+    public String detailsToString() {
+        StringBuilder sb = new StringBuilder("없음\n");
+        if (calculateTotalDiscount() != 0) {
+            sb.setLength(0);
+            appendDetails(sb);
+        }
+        return sb.toString();
+    }
+
+    public int calculatePresentationPrice() {
+        return details.get(Event.PRESENTATION);
+    }
+
+    public String presentationMenuToString() {
+        Event presentation = Event.PRESENTATION;
+        StringBuilder sb = new StringBuilder("없음\n");
+        if (details.get(presentation) != 0) {
+            sb.setLength(0);
+            sb.append(presentation.getTarget());
+            sb.append(" ");
+            sb.append("1개");
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    private void appendDetails(StringBuilder benefitDetail) {
+        for (Entry<Event, Integer> entry : details.entrySet()) {
+            String eventMessage = entry.getKey().getMessage();
+            int discount = entry.getValue();
+            if (discount != 0) {
+                benefitDetail.append(eventMessage);
+                benefitDetail.append(" ");
+                benefitDetail.append(String.format("%,d원", discount));
+                benefitDetail.append("\n");
+            }
+        }
+    }
+
+    private void initialBenefitDetails() {
+        Event.getAllEvent().forEach(event -> details.put(event, 0));
+    }
+
+}
