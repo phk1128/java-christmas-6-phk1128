ee485221c3c03c2f5a9e18dfda774c9837fe1735
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-13 15:50:51 +0900

    feat: OutputView 구현 완료

---
+++ src/main/java/christmas/view/output/OutputView.java
@@ -0,0 +1,58 @@
+package christmas.view.output;
+
+public class OutputView {
+
+    private static final String INITIATING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String REQUEST_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String REQUEST_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String PREVIEW = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private static final String ORDER_MENUS_DETAIL_TITLE = "<주문 메뉴>";
+    private static final String TOTAL_PRICE_TITLE = "<할인 전 총주문 금액>";
+    private static final String PRESENTATION_MENU_TITLE = "<증정 메뉴>";
+    private static final String BENEFIT_DETAIL_TITLE = "<혜택 내역>";
+    private static final String TOTAL_DISCOUNT_TITLE = "<총혜택 금액>";
+    private static final String TOTAL_PRICE_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    private static final String BADGE_TITLE = "<12월 이벤트 배지>";
+
+
+    public void printRequestDayMessage() {
+        System.out.println(INITIATING + "\n" + REQUEST_DAY);
+    }
+
+    public void printRequestOrderMessage() {
+        System.out.println(REQUEST_ORDER);
+    }
+
+    public void printPreviewMessage() {
+        System.out.println(PREVIEW);
+    }
+
+    public void printOrderMenusDetailMessage(String orderMenusDetail) {
+        System.out.println(ORDER_MENUS_DETAIL_TITLE + "\n" + orderMenusDetail);
+    }
+
+    public void printTotalPriceMessage(String totalPriceMessage) {
+        System.out.println(TOTAL_PRICE_TITLE + "\n" + totalPriceMessage);
+    }
+
+    public void printPresentationMenuMessage(String presentationMenuMessage) {
+        System.out.println(PRESENTATION_MENU_TITLE + "\n" + presentationMenuMessage);
+    }
+
+    public void printBenefitDetailMessage(String benefitDetailMessage) {
+        System.out.println(BENEFIT_DETAIL_TITLE + "\n" + benefitDetailMessage);
+    }
+
+    public void printTotalDiscountMessage(String totalDiscountMessage) {
+        System.out.println(TOTAL_DISCOUNT_TITLE + "\n" + totalDiscountMessage);
+    }
+
+    public void printTotalPriceAfterDiscount(String totalPriceAfterDiscount) {
+        System.out.println(TOTAL_PRICE_AFTER_DISCOUNT + "\n" + totalPriceAfterDiscount);
+    }
+
+    public void printBadge(String badge) {
+        System.out.print(BADGE_TITLE + "\n" + badge);
+    }
+
+}
