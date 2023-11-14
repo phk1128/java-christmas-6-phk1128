d30904c84ceabcfca73fcb7c8ab4bd19879af3d0
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-14 14:39:41 +0900

    refactor: ErrorMessage 상수화

---
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -0,0 +1,10 @@
+package christmas.constant;
+
+public class ErrorMessage {
+
+    public static final String WRONG_MENU = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String QUANTITY_OVER = "[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.";
+    public static final String ONLY_DRINK = "[ERROR] 음료만 주문 시, 주문할 수 없습니다.";
+    public static final String WRONG_DAY = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+}
