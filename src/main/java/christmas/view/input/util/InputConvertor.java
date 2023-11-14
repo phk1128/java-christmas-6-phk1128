428770535cf6fa8f6d700cb2fe9e06887a76ec60
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-14 14:43:23 +0900

    refactor(InputValidator, InputConvertor): 에러 메세지, 공백 상수화

--- src/main/java/christmas/view/input/util/InputConvertor.java
+++ src/main/java/christmas/view/input/util/InputConvertor.java
@@ -3,21 +3,23 @@ package christmas.view.input.util;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class InputConvertor {
 
     private static final String DELIMITER = ",";
+    private static final String SPACE = " ";
 
     private InputConvertor() {
 
     }
 
     public static List<String> convertStringToList(String input) {
-        input = input.replace(" ", "");
+        input = input.replace(SPACE, "");
         return Arrays.asList(input.split(DELIMITER));
     }
 
     public static int convertStringToInt(String input) {
-        input = input.replace(" ", "");
+        input = input.replace(SPACE, "");
         return Integer.parseInt(input);
     }
 
--- src/main/java/christmas/view/input/util/InputValidator.java
+++ src/main/java/christmas/view/input/util/InputValidator.java
@@ -1,5 +1,6 @@
 package christmas.view.input.util;
 
+import christmas.constant.ErrorMessage;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -20,13 +21,13 @@ public class InputValidator {
 
         public static void validateFormat(String menu) {
             if (!Pattern.matches("^[a-z|A-z|ㄱ-ㅎ|가-힣|0-9|\s]+-[0-9\s]+$", menu)) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException(ErrorMessage.WRONG_MENU);
             }
         }
 
         public static void validateQuantity(String menu) {
             if (menu.split("-")[1].equals("0")) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException(ErrorMessage.WRONG_MENU);
             }
 
         }
@@ -42,14 +43,14 @@ public class InputValidator {
 
         public static void validateFormat(String day) {
             if (!Pattern.matches("^[0-9\s]+$", day)) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException(ErrorMessage.WRONG_DAY);
             }
         }
 
         public static void validateRange(String day) {
             int dayNum = Integer.parseInt(day);
             if (dayNum <= 0 || dayNum > 31) {
-                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+                throw new IllegalArgumentException(ErrorMessage.WRONG_DAY);
             }
         }
     }
